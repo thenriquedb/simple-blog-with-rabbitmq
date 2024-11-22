68599a8ce50cb1ec23626e55fcab86f425095d19
Thiago Domingues <thenrique2012@gmail.com>
2024-11-22 18:33:12 -0300

    feat: configure article routes

--- blog/src/server.ts
+++ blog/src/server.ts
@@ -1,78 +1,84 @@
 import Fastify, { FastifyRequest } from 'fastify'
+import { RabbitMQ } from './shared/rabbitMQ'
+import { createArticle, createArticleSchema } from './controllers/create-article';
+import { findArticle, findArticleSchema } from './controllers/find-article';
+import { listArticles } from './controllers/list-articles';
 
 const fastify = Fastify({ logger: false })
 
-type Article = {
-  id: string;
-  title: string
-  content: string
-  category: string
+const channel = new RabbitMQ('amqp://guest:guest@localhost:5672');
+
+enum Category {
+  TECHNOLOGY = 'technology',
+  SPORTS = 'sports',
+  POLITICS = 'politics',
+  ECONOMY = 'economy',
 }
 
-const articles: Article[] = [];
-
-type CreateArticleBody = {
-  title: string
-  content: string
-  category: string
+type User = {
+  id: number;
+  name: string
+  email: string
 }
 
-const schema = {
-  body: {
-    type: 'object',
-    properties: {
-      title: { type: 'string' },
-      content: { type: 'string' },
-      category: { type: 'string' },
-    },
-    required: ['title', 'content', 'category']
-  }
+type Subscribers = {
+  userId: number;
+  category: string;
 }
-fastify.post('/article',
-  { schema },
-  async (request: FastifyRequest<{ Body: CreateArticleBody }>, reply) => {
-    const { title, content, category } = request.body;
 
-    const article = {
-      id: Date.now().toString(),
-      title, content,
-      category
+const users: User[] = [];
+const subscribers: Subscribers[] = [];
+
+type CreateUserBody = {
+  name: string
+  email: string
+}
+
+fastify.post('/user', async (request: FastifyRequest<{ Body: CreateUserBody }>, reply) => {
+  const { name, email } = request.body;
+
+  const user = {
+    id: users.length + 1,
+    name,
+    email
+  };
+
+  users.push(user);
+
+  return reply.status(200).send({
+    data: {
+      message: 'User created'
     }
-
-    articles.push(article);
-
-    return reply.status(200).send({
-      data: {
-        message: 'Article created',
-        article
-      }
-    });
-  });
-
-fastify.get('/article', async (_, reply) => {
-  return reply.status(200).send({
-    data: {
-      articles
-    },
   });
 });
 
-fastify.get('/article/:id', async (request: FastifyRequest<{ Params: { id: string } }>, reply) => {
-  const article = articles.find(article => article.id === request.params.id);
+fastify.post('/article', { schema: createArticleSchema }, createArticle);
+fastify.get('/article/:id', { schema: findArticleSchema }, findArticle);
+fastify.get('/article', listArticles);
 
-  if (!article) {
-    return reply.status(404).send({
-      error: {
-        message: 'Article not found',
-      },
-    });
+fastify.post('/subscribe', async (request: FastifyRequest<{ Body: { userId: number; category: Category } }>, reply) => {
+  const { userId, category } = request.body;
+
+  const subscriber = {
+    userId,
+    category
+  };
+
+  subscribers.push(subscriber);
+
+  return reply.status(200).send({
+    data: {
+      message: 'User subscribed'
+    }
+  });
+});
+
+(async () => {
+  try {
+    await channel.start();
+    await fastify.listen({ port: 3000 })
+    console.log('Server listening at 3000')
+  } catch (error) {
+    console.log('error', error);
   }
-
-  return reply.status(200).send({
-    data: {
-      article
-    },
-  });
-});
-
-fastify.listen({ port: 3000 });
+})();
