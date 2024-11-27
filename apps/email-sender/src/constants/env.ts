7be9291c2371d68ddf788f919dd041d515845fdb
Thiago Domingues <thenrique2012@gmail.com>
2024-11-27 00:07:57 -0300

    feat: email sender microservice

---
+++ apps/email-sender/src/constants/env.ts
@@ -0,0 +1,11 @@
+export default {
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  RABBITMQ_HOST: process.env.RABBITMQ_HOST,
+  RABBITMQ_PORT: process.env.RABBITMQ_PORT,
+  RABBITMQ_USERNAME: process.env.RABBITMQ_USERNAME,
+  RABBITMQ_PASSWORD: process.env.RABBITMQ_PASSWORD,
+}
---
+++ apps/email-sender/src/shared/rabbitMQ.ts
@@ -0,0 +1,57 @@
+import ampqlib, { Channel, Connection, Options } from 'amqplib';
+import env from '../constants/env';
+
+export class RabbitMQ {
+  private connection: Connection;
+  private channel: Channel;
+  private url: string;
+
+  constructor(url: string) {
+    this.url = url;
+  }
+
+  async start(): Promise<void> {
+    this.connection = await ampqlib.connect(this.url);
+    this.channel = await this.connection.createChannel();
+  }
+
+  async sendMessage(queue: string, message: string) {
+    this.channel.sendToQueue(queue, Buffer.from(message));
+    console.log(" [x] Sent %s", message);
+  }
+
+  async publishInExchange(exchange: string, routingKey: string, message: string, options?: Options.Publish) {
+    this.channel.assertExchange(
+      exchange,
+      'topic',
+      { durable: true }
+    );
+
+    return this.channel.publish(exchange, routingKey, Buffer.from(message), options)
+  }
+
+  async consume(queue: string, callback: (message: ampqlib.ConsumeMessage | null) => void) {
+    return this.channel.consume(queue, (message) => {
+      callback(message);
+    });
+  }
+
+  ack(message: ampqlib.ConsumeMessage) {
+    return this.channel.ack(message);
+  }
+
+  nack(message: ampqlib.ConsumeMessage, requeue: boolean) {
+    this.channel.nack(message, false, requeue);
+  }
+}
+
+function getRabbitMQUrl(): string {
+  return `amqp://${env.RABBITMQ_USERNAME}:${env.RABBITMQ_PASSWORD}@${env.RABBITMQ_HOST}:${env.RABBITMQ_PORT}`;
+}
+
+export async function getRabbitMQInstance(): Promise<RabbitMQ> {
+  const url = getRabbitMQUrl();
+  const channel = new RabbitMQ(url);
+  await channel.start();
+  return channel;
+}
